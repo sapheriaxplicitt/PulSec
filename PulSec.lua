65a59e316d9cc44e60b915d6b3225106868b2513
nitaybl <60972707+nitaybl@users.noreply.github.com>
2025-09-03 19:37:49 +0300

    Update PulSec.lua

--- PulSec.lua
+++ PulSec.lua
@@ -2,8 +2,7 @@
 local scripts = {
     [1234567890] = "https://yourdomain.com/script1.lua",
     [9876543210] = "https://yourdomain.com/script2.lua",
-    Default = "https://yourdomain.com/default.lua"
 }
 local id = game.PlaceId
-local url = scripts[id] or scripts.Default
+local url = scripts[id]
 loadstring(game:HttpGet(url))()
