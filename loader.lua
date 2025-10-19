89af87afbae5de866c767951d1e39b684511e1aa
sapheriaxplicitt <sapheria@saph.cc>
2025-10-19 21:24:33 +0900

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
--- Master Loader
-local scripts = {
-    [8657806289] = "https://raw.githubusercontent.com/nitaybl/PulSec/refs/heads/main/dummiesvsnoobs.lua",
-    [121366332180773] = "https://yourdomain.com/script2.lua",
-}
-local id = game.PlaceId
-local url = scripts[id]
-loadstring(game:HttpGet(url))()
